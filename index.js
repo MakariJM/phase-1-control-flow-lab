cb0254b8de9d56867ebb5489670a94ae274bc1d2
JuliusMakari <muragejmakari@gmail.com>
2023-12-13 13:50:16 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let answer;
+
+  if (feet<=400) {
+    return "This one is on me!";
+  } else if (feet > 2500) {
+    return "No can do.";
+  }else if (feet >=2000) {
+    return "I will gladly take your thirty bucks.";
+  }
 }
 
 function ternaryCheckCity(){
   // Write your code here!
+  let result;
+
+  city === "NYC" ? (result = "ok, sounds good."): (result = "No go.");
+
+  return result;
 }
 
 function switchOnCharmFromTip(){
   // Write your code here!
+
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.":
+      break
+    case "not as generous":
+      return "Thank you.";
+      break;
+    default:
+      return "Bye.";
+      break;
+  }
 }
